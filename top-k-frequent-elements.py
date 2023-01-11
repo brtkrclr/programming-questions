e94ec86eec14817679a0df88e6a9a5bf15a0e5a6
berat <berat.karacalar@optiim.com>
2023-01-11 22:18:22 +0000

    Solved

---
+++ top-k-frequent-elements.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+		Example 1:
+
+		Input: nums = [1,1,1,2,2,3], k = 2
+		Output: [1,2]
+		Example 2:
+
+		Input: nums = [1], k = 1
+		Output: [1]
+		 
+
+		Constraints:
+
+		1 <= nums.length <= 105
+		-104 <= nums[i] <= 104
+		k is in the range [1, the number of unique elements in the array].
+		It is guaranteed that the answer is unique.
+        """
+        d={}
+        for i in nums:
+            if i in d:
+              d[i]+=1
+            else:
+              d[i]=1
+        sd=sorted(d,key=d.get,reverse=True)
+        return sd[:k]
