2ace302de7b27670d5d70892990af07ce791b963
berat <berat.karacalar@optiim.com>
2024-06-03 20:56:23 +0100

    Description Added

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -4,6 +4,26 @@ from typing import List
 class Solution:
     @staticmethod
     def isValid(s: str) -> bool:
+        '''
+        Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+        An input string is valid if:
+        Open brackets must be closed by the same type of brackets.
+        Open brackets must be closed in the correct order.
+        Every close bracket has a corresponding open bracket of the same type.
+
+        Example 1:
+        Input: s = "()"
+        Output: true
+
+        Example 2:
+        Input: s = "()[]{}"
+        Output: true
+
+        Example 3:
+        Input: s = "(]"
+        Output: false
+
+        '''
         arr = []
         paranthesis = {"(": ")", "[": "]", "{": "}"}
         for i in s:
